81dd476a127d35e1be34d907badf4c00c11b7595
Raul Morales <irmoralesb@hotmail.com>
2026-01-01 19:03:43 -0600

    Refactoring Project Structure

---
+++ llm_tools/openai_tools/client_provider.py
@@ -0,0 +1,16 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+_client: OpenAI = None
+
+
+def get_client() -> OpenAI:
+    global _client
+    if _client is None:
+        api_key = os.environ.get("OPENAI_API_KEY")
+        if not api_key:
+            raise ValueError("OPENAI_API_KEY environment variable is not set")
+        _client = OpenAI(api_key=api_key)
+    return _client
